2cd4477641d4df968ee2c3b597d268203f3261f5
MatheusESouza21 <bmakgacha@gmail.com>
2025-03-05 20:42:54 -0300

    alt

--- ex03_MatheusEduardo.py
+++ ex03_MatheusEduardo.py
@@ -4,13 +4,13 @@ pergunte se deseja adicionar outro. Se o fizer, permita que adicione mais nomes 
 uma vez que o usuário tenha completado sua lista de nomes, exiba a lista completa e peça que ele digite um dos nomes da lista. Exiba a posição desse nome na lista. 
 Pergunte ao usuário se ele ainda deseja que essa pessoa venha à festa. Se ele responder "não", exclua essa entrada da lista e exiba a lista novamente.
 """
-nomes = ()
+nomes = []
 adicionar_nomes = list(nomes)
 
 for i in range(3):
     nome = input('Insira o {}° nome: '.format(i+1))
     adicionar_nomes.append(nome)
-    nomes = tuple(adicionar_nomes)
+    nomes = (adicionar_nomes)
 
 confirmacao = input('Deseja adicionar mais um nome? (S ou N) ').upper()
 
@@ -18,15 +18,27 @@ if confirmacao == 'S':
     while confirmacao == 'S':
         nome = input('Insira o nome: ')
         adicionar_nomes.append(nome)
-        nomes = tuple(adicionar_nomes)
-        confirmacao = input('Deseja adicionar mais um nome? (S ou N) ').upper()
+        nomes = (adicionar_nomes)
+        confirmacao = input('Deseja adicionar mais um nome? (S ou N): ').upper()
 
 print('Você convidou {} pessoas para a festa'.format(len(nomes)))
 
 print(nomes)
-nome = input('Digite um dos nomes da lista: ').lower()
-if nome in nomes:
-    
+escolhido = input('Digite um dos nomes da lista: ')
+if escolhido in nomes:
+    for i in range(len(nomes)):
+        if nomes[i] == escolhido:
 
+            print('A posição de {} é {}'.format(nomes[i],i))
 
-    
+else:
+    print('Esse nome não está na lista.')
+
+confirmacao = input('Você ainda deseja que {} venha a festa? (sim/nao): '.format(escolhido)).lower()
+if confirmacao == 'nao':
+    nomes.remove(escolhido)
+
+print(nomes)
+
+print('Matheus Eduardo Souza')
+
